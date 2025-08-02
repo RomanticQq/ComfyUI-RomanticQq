cab2662eb1e032cbca846fafbe94a66391c4d18a
RomanticQq <3236997288@qq.com>
2025-08-02 15:08:34 +0800

    add split filter concat node

--- split_filter_concat.py
+++ split_filter_concat.py
@@ -19,7 +19,7 @@ class SplitFilterConcat:
             "required": {
                 "text": ("STRING",),
                 "split": ("STRING",{"default": None}),
-                "filter": ("LIST",{"default": None}),
+                "filter": ("STRING",{"default": None}),
                 "concat": ("STRING",{"default": None}),
             },
         }
@@ -27,19 +27,33 @@ class SplitFilterConcat:
     RETURN_TYPES = ("STRING",)
     FUNCTION = "test"
     CATEGORY = "RomanticQq"
-    def test(self, text, split=None, flter=None, concat=None):
+    def test(self, text, split=None, filter=None, concat=None):
         print("text: ", text)
+        text = text.strip()
+        print("strip text: ", text)
+        res_text = ""
         try:
             if split is None or filter is None or concat is None:
                 res_text = text
             else:
+                filter = filter.split(",") if filter else None
+                if isinstance(filter, str):
+                    filter = [filter]
                 split_texts = text.split(split)
+                print("split_texts: ", split_texts)
                 new_texts = []
                 for t in split_texts:
-                    if flter is not None and t in flter:
-                        continue
-                    new_texts.append(t)
+                    t = t.strip()
+                    flag = True
+                    for f in filter:
+                        if f in t or t.strip() == "":
+                            flag = False
+                            break
+                    if flag:
+                        new_texts.append(t)
+                print("new_texts: ", new_texts)
                 res_text = concat.join(new_texts)+concat
+            res_text = res_text.strip()
             print("res_text: ", res_text)
         except Exception as e:
             print(e)
