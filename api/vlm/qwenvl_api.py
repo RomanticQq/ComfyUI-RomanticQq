8292cf63018ddcd4073c5c3f3ff33baf27f8e12b
RomanticQq <3236997288@qq.com>
2025-08-25 10:52:23 +0800

    update

--- api/vlm/qwenvl_api.py
+++ api/vlm/qwenvl_api.py
@@ -11,6 +11,8 @@ from datetime import datetime, timedelta
 
 class QWENVL:
     def __init__(self):
+
+
         self.tmp_dir = os.path.join(os.path.dirname(__file__), "tmp")
         if not os.path.exists(self.tmp_dir):
             os.makedirs(self.tmp_dir)
